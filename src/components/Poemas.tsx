ce28d111e04005befa63f0f25e1c48ccd2d271cf
alanlasco <61526254+alanlasco@users.noreply.github.com>
2023-08-08 20:45:13 -0300

    agregar leer mas/menos, correciones

--- src/components/Poemas.tsx
+++ src/components/Poemas.tsx
@@ -1,26 +1,38 @@
-import React from 'react'
+import {useState} from 'react'
 import { usePoemas } from '../hooks/usePoemas';
 import { reqPoema } from '../interfaces/reqPoema';
+import '../stylesheets/Poema.css';
 
 
 export const Poemas = () => {
-    const {poemas} = usePoemas();
-    const renderPoemas = (poema: reqPoema) =>{
-        return(
-        <div key={poema.id_poema}>
-            <h4>{poema.nombre}</h4>
-            <p>{poema.poema}</p>
-            <p>{poema.fecha}</p>
-        </div >
-        )
+  const { poemas } = usePoemas();
+  const [showFullText, setShowFullText] = useState<boolean>(false);
 
-    }
+  const togglePoema = () => {
+    setShowFullText(!showFullText);
+  };
+
+  const renderPoemas = (poema: reqPoema) => {
+    let fulltext= poema.poema;
+    let partialtext = poema.poema.slice(0, 150);
+    const currentPoema = showFullText ? fulltext : partialtext+'...';
+    const poemaFormatted = currentPoema.replace(/\n/g, "<br>")
+    return (
+      <div className='poemas-container' key={poema.id_poema}>
+        <h4 className='titulo'>{poema.nombre}</h4>
+        <br />
+        <p style={{whiteSpace: 'pre-line'}} className='poema toggled'>{currentPoema}</p>
+        <p className='fecha'>{poema.fecha}</p>
+        <button onClick={togglePoema}>
+          {showFullText ? 'Leer menos' : 'Leer más'}
+        </button>
+      </div>
+    );
+  };
+
   return (
     <>
-    
-    {
-    poemas.map(renderPoemas)
-    }
+      {poemas.map(renderPoemas)}
     </>
-  )
-}
+  );
+};
