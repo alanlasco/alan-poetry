02cc4a75c6b0f1e84e0937339e2043c43c814499
alanlasco <61526254+alanlasco@users.noreply.github.com>
2023-08-12 02:14:38 -0300

    blurrear texto, prueba animaciones

--- src/components/Poemas.tsx
+++ src/components/Poemas.tsx
@@ -15,7 +15,23 @@ export const Poemas = () => {
     }));
   };
   const getPoemaClass = (poemaId: number) => {
-    return `${poemaStates[poemaId] ? '' : 'toggled'}`;
+    return `${poemaStates[poemaId] ? 'btn-toggle-poema' : 'btn-toggled-poema'}`;
+  };
+  const getPoemaClassFecha = (poemaId: number) => {
+    return `${poemaStates[poemaId] ? 'fecha' : 'fecha-none'}`;
+  };
+  const getPoemaClassBlurred = (poemaId: number) => {
+    return `${poemaStates[poemaId] ? 'poema' : 'poema-blurred'}`;
+  };
+  const containerExpanded = (poemaId: number) => {
+    return `${poemaStates[poemaId] ? '' : 'container-expanded'}`;
+  };  
+  
+/*********/
+  const [isHiddenTextVisible, setIsHiddenTextVisible] = useState(false);
+
+  const toggleText = () => {
+    setIsHiddenTextVisible(!isHiddenTextVisible);
   };
 
   const renderPoemas = (poema: reqPoema) => {
@@ -24,30 +40,44 @@ export const Poemas = () => {
     const truncatedText = poema.poema.slice(0, 150);
     const currentPoema = isExpanded ? poema.poema : truncatedText + '...';
 
-    return (
-      <>
-      <div className={getPoemaClass(poema.id_poema)}>
-      <div className='poemas-container 'key={poema.id_poema}>
-        <h4 className='titulo'>{poema.nombre}</h4>
-        <br />
-        <p style={{ whiteSpace: 'pre-line' }} className='poema'>{currentPoema}</p>
-        <p className='fecha'>{poema.fecha}</p>
-        <div className='btn-toggle' onClick={() => togglePoema(poema.id_poema)}>
-          {isExpanded ? 'Leer menos' : 'Leer más'}
-        </div>
-      </div>
-      </div>
-      <br /><br />
-      </>
-    );
+    
+  return (
+    <div key={poema.id_poema}>
+    <div key={poema.id_poema} className={`poemas-container ${containerExpanded}`}>
+      <h4 className='titulo'>{poema.nombre}</h4>
+      <br />
+      <p
+        key={`poema-${poema.id_poema}`}
+        style={{ whiteSpace: 'pre-line' }}
+        className={getPoemaClassBlurred(poema.id_poema)}
+      >
+        {currentPoema}
+      </p>
+      <p key={`fecha-${poema.id_poema}`} className={getPoemaClassFecha(poema.id_poema)}>
+        {poema.fecha}
+      </p>
+      <button
+        key={`button-${poema.id_poema}`}
+        className={getPoemaClass(poema.id_poema)}
+        onClick={() => togglePoema(poema.id_poema)}
+      >
+        {isExpanded ? 'Leer menos' : 'Leer más'}
+      </button>
+    </div>
+    <br /><br />
+    </div>
+  );
   };
 
   return (
     <>
+
       {poemas.map(renderPoemas)}
     </>
   );
 };
+
+
 // En este código, hemos creado un estado poemaStates utilizando el estado de useState.Este estado es un objeto d
 
 
