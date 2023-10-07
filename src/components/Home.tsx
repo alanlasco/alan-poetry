7b03333ace96dd7c793c857954a733205ae1dd7e
alanlasco <61526254+alanlasco@users.noreply.github.com>
2023-10-06 23:31:27 -0300

    estilos para Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,8 +11,22 @@ export const Home = () => {
     exit={{opacity: 0}}
     
     >
+        <div className='contenedor-bienvenida'>
         <h1 className='bienvenida'>Bienvenidos</h1>
-
+        <br />
+        <p>Poemario de Alan Lasco</p>
+        <br />
+        <div className='socials-contenedor'>
+          <a className="socials" href="https://github.com">
+            <img src={githubImage} alt="GitHub" />
+            GitHub
+          </a>
+          <a className="socials" href="https://linkedin.com">
+            <img src={linkdnImage} alt="LinkedIn" />
+            LinkedIn
+          </a>
+        </div>
+        </div>
 
 
     </motion.div>
