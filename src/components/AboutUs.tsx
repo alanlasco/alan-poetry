673232c7f482d014725848f3754246c5ab4e14e0
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-03-09 17:27:17 -0300

    adding text about alan

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -22,24 +22,40 @@ function AboutUs() {
             <span className="about-alan">Sobre Alan Lasco</span>
           </div>
         </div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
-        blanditiis recusandae molestiae quas ipsum saepe. Maiores cupiditate
-        deleniti obcaecati voluptatibus. Omnis soluta rem praesentium
-        voluptates? Distinctio, magni non! Eius, sint. Molestias alias corrupti
-        aut sed a et ipsam suscipit fugiat voluptates deserunt aspernatur, error
-        quibusdam mollitia placeat omnis veritatis aliquid laborum modi
-        voluptate eos. Incidunt porro sit repudiandae recusandae. Qui. Dolorem
-        dolor facere cum quae, a voluptatibus obcaecati. A odit maxime at
-        consequuntur cumque! Beatae, error consequuntur quia corrupti facilis
-        unde modi, asperiores omnis nam pariatur perspiciatis nemo facere ipsam!
-        Mollitia, nobis aperiam! Voluptatem necessitatibus labore, voluptatibus
-        numquam consequatur reiciendis vero adipisci officiis sit maiores, natus
-        neque distinctio at illo aut exercitationem impedit. Quasi officiis
-        ratione totam eaque? Velit, tempore? Amet repellat non, repudiandae,
-        perferendis nobis quaerat similique accusamus reiciendis veniam nihil
-        necessitatibus neque iusto voluptatem hic suscipit. Nulla deserunt enim
-        voluptas excepturi cupiditate sed quia tempora facilis explicabo
-        officiis.
+        <div className="about-alan-text">
+          <p>
+            Mi pasión por la escritura comenzó como muchas otras historias,
+            experiencias y pasiones: en el colegio. Gracias a las clases de
+            literatura, empezamos a sumergirnos en un lugar tan profundo que ha
+            llegado a hacer llorar a los compañeros más duros, aquellos que se
+            sientan en los últimos bancos. Nos adentramos en el mundo de la
+            poesía.
+            <br />
+            <br />
+            Me atrajo tanto ese mundo que escribía poemas para mostrarle a la
+            profesora, aunque no fuera una actividad programada por ella.
+            Comenzamos leyendo un libro que recopilaba muchos poemas de
+            distintos autores, de los cuales no recuerdo sus nombres. Luego,
+            empezamos a escribir, haciendo collages con partes de estos poemas.
+            Después, sentí la curiosidad de investigar sobre poetas. En ese
+            momento, recordé la historia de César González (escritor, poeta,
+            cineasta, filósofo) e inmediatamente fui a comprarme alguno de sus
+            libros, y me encantaron.
+            <br />
+            <br />
+            El proyecto final del curso consistía en armar un pequeño libro de
+            poesía con la recopilación de poemas de todos los alumnos. El libro
+            se llamó "Sin Principio ni Final". Lo conservo con mucho cariño, al
+            igual que una frase que nos dijo la profesora: "Cuando escriben,
+            ustedes son su propio Dios". Seamos libres al escribir y expresar
+            todo lo que está en nuestra mente. Sean su propio dios.
+            <br />
+            <br />
+            Espero que disfruten de mis poemas y escritos. ¡Espero que
+            encuentren en ellos alguna frase que los transporte a un lugar
+            especial!
+          </p>
+        </div>
       </div>
     </motion.div>
   );
