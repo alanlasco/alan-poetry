b4e173979b177f7d79b5acef1df278cdfa86c0e2
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-08-11 16:57:05 -0300

    se invirtieron los array de poemas

--- src/components/Poemas.tsx
+++ src/components/Poemas.tsx
@@ -1,36 +1,36 @@
-
-import { useState } from 'react'
-import { usePoemas } from '../hooks/usePoemas';
-import { reqPoema } from '../interfaces/reqPoema';
-import '../stylesheets/Poema.css';
+import { useState } from "react";
+import { usePoemas } from "../hooks/usePoemas";
+import { reqPoema } from "../interfaces/reqPoema";
+import "../stylesheets/Poema.css";
 
 export const Poemas = () => {
   const { poemas } = usePoemas();
 
   // Creamos un objeto para almacenar el estado de cada poema individualmente
-  const [poemaStates, setPoemaStates] = useState<{ [key: number]: boolean }>({});
+  const [poemaStates, setPoemaStates] = useState<{ [key: number]: boolean }>(
+    {}
+  );
   const togglePoema = (poemaId: number) => {
-    setPoemaStates(prevState => ({
+    setPoemaStates((prevState) => ({
       ...prevState,
-      [poemaId]: !prevState[poemaId]
+      [poemaId]: !prevState[poemaId],
     }));
   };
-  
 
   const getPoemaClass = (poemaId: number) => {
-    return `${poemaStates[poemaId] ? 'btn-toggle-poema' : 'btn-toggled-poema'}`;
+    return `${poemaStates[poemaId] ? "btn-toggle-poema" : "btn-toggled-poema"}`;
   };
   const getPoemaClassFecha = (poemaId: number) => {
-    return `${poemaStates[poemaId] ? 'fecha' : 'fecha-none'}`;
+    return `${poemaStates[poemaId] ? "fecha" : "fecha-none"}`;
   };
   const getPoemaClassBlurred = (poemaId: number) => {
-    return `${poemaStates[poemaId] ? 'poema' : 'poema-blurred'}`;
+    return `${poemaStates[poemaId] ? "poema" : "poema-blurred"}`;
   };
   const containerExpanded = (poemaId: number) => {
-    return `${poemaStates[poemaId] ? '' : 'container-expanded'}`;
-  };  
-  
-/*********/
+    return `${poemaStates[poemaId] ? "" : "container-expanded"}`;
+  };
+
+  /*********/
   // const [isHiddenTextVisible, setIsHiddenTextVisible] = useState(false);
 
   // const toggleText = () => {
@@ -38,44 +38,47 @@ export const Poemas = () => {
   // };
 
   const renderPoemas = (poema: reqPoema) => {
-    
     const isExpanded = poemaStates[poema.id_poema] || false;
     const truncatedText = poema.poema.slice(0, 150);
-    const currentPoema = isExpanded ? poema.poema : truncatedText + '...';
-    
-    
-  return (
-    <div className='todosAnimation' key={poema.id_poema}>
-    <div key={poema.id_poema} className={`poemas-container ${containerExpanded}`}>
-      <h4 className='titulo'>{poema.nombre}</h4>
-        <br />
-        <p className='separador'></p>
-        <br />
-<p
-    key={`poema-${poema.id_poema}`}
-    style={{ whiteSpace: 'pre-line' }}
-    className={`${
-        getPoemaClassBlurred(poema.id_poema)} ${
-        poemaStates[poema.id_poema] ? 'poema-expanded' : ''
-    }`}
->
-    {currentPoema}
-</p>
-      <p key={`fecha-${poema.id_poema}`} className={getPoemaClassFecha(poema.id_poema)}>
-        {poema.fecha}
-      </p>
-      <button
-        key={`button-${poema.id_poema}`}
-        className={getPoemaClass(poema.id_poema)}
-        onClick={() => togglePoema(poema.id_poema)}
-      >
-        {isExpanded ? 'Leer menos' : 'Leer más'}
-      </button>
-    </div>
-    <br /><br />
-    </div>
-  );
+    const currentPoema = isExpanded ? poema.poema : truncatedText + "...";
 
+    return (
+      <div className="todosAnimation" key={poema.id_poema}>
+        <div
+          key={poema.id_poema}
+          className={`poemas-container ${containerExpanded}`}
+        >
+          <h4 className="titulo">{poema.nombre}</h4>
+          <br />
+          <p className="separador"></p>
+          <br />
+          <p
+            key={`poema-${poema.id_poema}`}
+            style={{ whiteSpace: "pre-line" }}
+            className={`${getPoemaClassBlurred(poema.id_poema)} ${
+              poemaStates[poema.id_poema] ? "poema-expanded" : ""
+            }`}
+          >
+            {currentPoema}
+          </p>
+          <p
+            key={`fecha-${poema.id_poema}`}
+            className={getPoemaClassFecha(poema.id_poema)}
+          >
+            {poema.fecha}
+          </p>
+          <button
+            key={`button-${poema.id_poema}`}
+            className={getPoemaClass(poema.id_poema)}
+            onClick={() => togglePoema(poema.id_poema)}
+          >
+            {isExpanded ? "Leer menos" : "Leer más"}
+          </button>
+        </div>
+        <br />
+        <br />
+      </div>
+    );
   };
 
   const [startIndex, setStartIndex] = useState(0);
@@ -93,26 +96,27 @@ export const Poemas = () => {
 
   return (
     <>
-      {currentPoemas.map(renderPoemas)}
-    <div>
-        <button className='button_slide slide_down' onClick={handlePreviousClick} disabled={startIndex === 0}>
-        {'<< Anterior'}
-      </button>
-      <button className='button_slide slide_down' onClick={handleNextClick} disabled={startIndex + itemsPerPage >= poemas.length}>
-        {'Siguiente >>'}
-      </button>
+      {currentPoemas.reverse().map(renderPoemas)}
+      <div>
+        <button
+          className="button_slide slide_down"
+          onClick={handlePreviousClick}
+          disabled={startIndex === 0}
+        >
+          {"<< Anterior"}
+        </button>
+        <button
+          className="button_slide slide_down"
+          onClick={handleNextClick}
+          disabled={startIndex + itemsPerPage >= poemas.length}
+        >
+          {"Siguiente >>"}
+        </button>
       </div>
     </>
   );
 };
 
-
 // En este código, hemos creado un estado poemaStates utilizando el estado de useState.Este estado es un objeto d
 
-
 // En este ejemplo, he reorganizado el código para usar el estado startIndex para rastrear qué grupo de poemas se debe mostrar.Cuando haces clic en el botón "Siguiente", se actualiza startIndex, lo que a su vez actualiza los poemas que se renderizan en ese momento.
-
-
-
-
-
--- src/components/PoemasFavoritos.tsx
+++ src/components/PoemasFavoritos.tsx
@@ -1,114 +1,121 @@
-
-import { useState } from 'react'
-import { usePoemasFavoritos } from '../hooks/usePoemasFavoritos';
-import { reqPoema } from '../interfaces/reqPoema';
-import '../stylesheets/Poema.css';
+import { useState } from "react";
+import { usePoemasFavoritos } from "../hooks/usePoemasFavoritos";
+import { reqPoema } from "../interfaces/reqPoema";
+import "../stylesheets/Poema.css";
 
 export const PoemasFavoritos = () => {
-    const { poemasFavoritos } = usePoemasFavoritos();
+  const { poemasFavoritos } = usePoemasFavoritos();
 
-    // Creamos un objeto para almacenar el estado de cada poema individualmente
-    const [poemaStates, setPoemaStates] = useState<{ [key: number]: boolean }>({});
-    const togglePoema = (poemaId: number) => {
-        setPoemaStates(prevState => ({
-            ...prevState,
-            [poemaId]: !prevState[poemaId]
-        }));
-    };
-    const getPoemaClass = (poemaId: number) => {
-        return `${poemaStates[poemaId] ? 'btn-toggle-poema-v' : 'btn-toggled-poema-v'}`;
-    };
-    const getPoemaClassFecha = (poemaId: number) => {
-        return `${poemaStates[poemaId] ? 'fecha' : 'fecha-none'}`;
-    };
-    const getPoemaClassBlurred = (poemaId: number) => {
-        return `${poemaStates[poemaId] ? 'poema' : 'poema-blurred'}`;
-    };
-    const containerExpanded = (poemaId: number) => {
-        return `${poemaStates[poemaId] ? '' : 'container-expanded'}`;
-    };
+  // Creamos un objeto para almacenar el estado de cada poema individualmente
+  const [poemaStates, setPoemaStates] = useState<{ [key: number]: boolean }>(
+    {}
+  );
+  const togglePoema = (poemaId: number) => {
+    setPoemaStates((prevState) => ({
+      ...prevState,
+      [poemaId]: !prevState[poemaId],
+    }));
+  };
+  const getPoemaClass = (poemaId: number) => {
+    return `${
+      poemaStates[poemaId] ? "btn-toggle-poema-v" : "btn-toggled-poema-v"
+    }`;
+  };
+  const getPoemaClassFecha = (poemaId: number) => {
+    return `${poemaStates[poemaId] ? "fecha" : "fecha-none"}`;
+  };
+  const getPoemaClassBlurred = (poemaId: number) => {
+    return `${poemaStates[poemaId] ? "poema" : "poema-blurred"}`;
+  };
+  const containerExpanded = (poemaId: number) => {
+    return `${poemaStates[poemaId] ? "" : "container-expanded"}`;
+  };
 
-    /*********/
-    // const [isHiddenTextVisible, setIsHiddenTextVisible] = useState(false);
+  /*********/
+  // const [isHiddenTextVisible, setIsHiddenTextVisible] = useState(false);
 
-    //  const toggleText = () => {
-    //     setIsHiddenTextVisible(!isHiddenTextVisible);
-    // };
+  //  const toggleText = () => {
+  //     setIsHiddenTextVisible(!isHiddenTextVisible);
+  // };
 
-    const renderPoemas = (poema: reqPoema) => {
-
-        const isExpanded = poemaStates[poema.id_poema] || false;
-        const truncatedText = poema.poema.slice(0, 150);
-        const currentPoema = isExpanded ? poema.poema : truncatedText + '...';
-        
-
-        return (
-            <div className='favouriteAnimation' key={poema.id_poema}>
-                <div key={poema.id_poema} className={`poemas-container ${containerExpanded}`}>
-                    <h4 className='titulo'>{poema.nombre}</h4>
-                    <br />
-                    <p className='separador-verde'></p>
-                    <br />
-                    <p
-                        key={`poema-${poema.id_poema}`}
-                        style={{ whiteSpace: 'pre-line' }}
-                        className={getPoemaClassBlurred(poema.id_poema)}
-                    >
-                        {currentPoema}
-                    </p>
-                    <p key={`fecha-${poema.id_poema}`} className={getPoemaClassFecha(poema.id_poema)}>
-                        {poema.fecha}
-                    </p>
-                    <button
-                        key={`button-${poema.id_poema}`}
-                        className={getPoemaClass(poema.id_poema)}
-                        onClick={() => togglePoema(poema.id_poema)}
-                    >
-                        {isExpanded ? 'Leer menos' : 'Leer más'}
-                    </button>
-                    <br />
-                    <br />
-                </div>
-                <br /><br />
-            </div>
-        );
-
-    };
-
-    const [startIndex, setStartIndex] = useState(0);
-    const itemsPerPage = 3;
-
-    const handleNextClick = () => {
-        setStartIndex(startIndex + itemsPerPage);
-    };
-
-    const handlePreviousClick = () => {
-        setStartIndex(startIndex - itemsPerPage);
-    };
-
-    const currentPoemas = poemasFavoritos.slice(startIndex, startIndex + itemsPerPage);
+  const renderPoemas = (poema: reqPoema) => {
+    const isExpanded = poemaStates[poema.id_poema] || false;
+    const truncatedText = poema.poema.slice(0, 150);
+    const currentPoema = isExpanded ? poema.poema : truncatedText + "...";
 
     return (
-        <>
-            {currentPoemas.map(renderPoemas)}
-            <div>
-                <button onClick={handlePreviousClick} disabled={startIndex === 0}>
-                    Anterior
-                </button>
-                <button onClick={handleNextClick} disabled={startIndex + itemsPerPage >= poemasFavoritos.length}>
-                    Siguiente
-                </button>
-            </div>
-        </>
+      <div className="favouriteAnimation" key={poema.id_poema}>
+        <div
+          key={poema.id_poema}
+          className={`poemas-container ${containerExpanded}`}
+        >
+          <h4 className="titulo">{poema.nombre}</h4>
+          <br />
+          <p className="separador-verde"></p>
+          <br />
+          <p
+            key={`poema-${poema.id_poema}`}
+            style={{ whiteSpace: "pre-line" }}
+            className={getPoemaClassBlurred(poema.id_poema)}
+          >
+            {currentPoema}
+          </p>
+          <p
+            key={`fecha-${poema.id_poema}`}
+            className={getPoemaClassFecha(poema.id_poema)}
+          >
+            {poema.fecha}
+          </p>
+          <button
+            key={`button-${poema.id_poema}`}
+            className={getPoemaClass(poema.id_poema)}
+            onClick={() => togglePoema(poema.id_poema)}
+          >
+            {isExpanded ? "Leer menos" : "Leer más"}
+          </button>
+          <br />
+          <br />
+        </div>
+        <br />
+        <br />
+      </div>
     );
-};
+  };
 
+  const [startIndex, setStartIndex] = useState(0);
+  const itemsPerPage = 3;
+
+  const handleNextClick = () => {
+    setStartIndex(startIndex + itemsPerPage);
+  };
+
+  const handlePreviousClick = () => {
+    setStartIndex(startIndex - itemsPerPage);
+  };
+
+  const currentPoemas = poemasFavoritos.slice(
+    startIndex,
+    startIndex + itemsPerPage
+  );
+
+  return (
+    <>
+      {currentPoemas.reverse().map(renderPoemas)}
+      <div>
+        <button onClick={handlePreviousClick} disabled={startIndex === 0}>
+          Anterior
+        </button>
+        <button
+          onClick={handleNextClick}
+          disabled={startIndex + itemsPerPage >= poemasFavoritos.length}
+        >
+          Siguiente
+        </button>
+      </div>
+    </>
+  );
+};
 
 // En este código, hemos creado un estado poemaStates utilizando el estado de useState.Este estado es un objeto d
 
-
 // En este ejemplo, he reorganizado el código para usar el estado startIndex para rastrear qué grupo de poemas se debe mostrar.Cuando haces clic en el botón "Siguiente", se actualiza startIndex, lo que a su vez actualiza los poemas que se renderizan en ese momento.
-
-
-
-
