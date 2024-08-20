7889d11dba889e562b513fe454c9a5a4031e6695
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-08-20 18:43:28 -0300

    styling layout

--- src/components/showPoema.tsx
+++ src/components/showPoema.tsx
@@ -2,8 +2,19 @@ import { useState } from "react";
 import "../stylesheets/Poema.css";
 import { poemProps } from "../interfaces/inPoemProps";
 export const showPoema = ({ id_poema, titulo, texto, fecha }: poemProps) => {
-  return;
-  <>
-    <div>showPoema</div>
-  </>;
+  return (
+    <>
+      <div className="contenedorPoema">
+        <div className="headerPoema">
+          <h4 className="tituloPoema">{titulo}</h4>
+
+          <p className="separador"></p>
+        </div>
+        <p className="textoPoema">{texto}</p>
+        <div className="footerPoema">
+          <p className="fechaPoema">{fecha}</p>
+        </div>
+      </div>
+    </>
+  );
 };
