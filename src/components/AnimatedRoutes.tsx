fdf2a3d8443a59df204cd4d8cf5cce4c650e84d7
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-08-20 15:48:50 -0300

    setting off show poema

--- src/components/AnimatedRoutes.tsx
+++ src/components/AnimatedRoutes.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
+import React from "react";
 import {
-    BrowserRouter,
-    Routes,
-    Route,
-    Link,
-    useLocation
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useLocation,
 } from "react-router-dom";
-import { Home } from './Home';
-import Poemario from './Poemario';
-import AboutUs from './AboutUs';
-import Contact from './Contact';
-import { AnimatePresence } from 'framer-motion';
+import { Home } from "./Home";
+import Poemario from "./Poemario";
+import AboutUs from "./AboutUs";
+import Contact from "./Contact";
+import { AnimatePresence } from "framer-motion";
 
 export const AnimatedRoutes = () => {
-    const location = useLocation();
+  const location = useLocation();
   return (
-      <AnimatePresence>    
-        <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<Home />} />
-          <Route path="/poemario" element={<Poemario />} />
-          <Route path="/sobremi" element={<AboutUs />} />
-          <Route path="/contacto" element={<Contact />} />
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/poemario" element={<Poemario />} />
+        <Route path="/sobremi" element={<AboutUs />} />
+        <Route path="/contacto" element={<Contact />} />
       </Routes>
-      </AnimatePresence>
-  )
-}
+    </AnimatePresence>
+  );
+};
---
+++ src/components/showPoema.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import "../stylesheets/Poema.css";
+
+export const showPoema = () => {
+  return;
+  <>
+    <div>showPoema</div>
+  </>;
+};
